0c68c5b7c0cfffdcaf8d7990130fb742f57981e3
srijansa <159839387+srijansa@users.noreply.github.com>
2024-05-20 13:13:24 -0400

    Added messages

--- Arrays/118_Pascals_Triangle_Easy.py
+++ Arrays/118_Pascals_Triangle_Easy.py
@@ -2,6 +2,11 @@ import sys
 from typing import List
 #Use the following to execute: echo 5 | python 118_Pascals_Triangle_Easy.py
 class Solution: 
+    """
+    Remeber to add a zero in the end and the start of every row.
+    Computing the next row based on the previous row.
+    result[-1] gives the last row of the 2D Matrix
+    """
     def generate(self, numRows: int) -> List[List[int]]:
         if numRows == 0:
             return []
